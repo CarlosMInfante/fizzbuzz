6e4d9c31f44397f3dadcd24993b36740799f6256
CarlosMInfante <83223613+CarlosMInfante@users.noreply.github.com>
2021-11-12 16:23:59 -0500

    Working on code

--- js/site.js
+++ js/site.js
@@ -11,39 +11,50 @@ function getValues() {
     buzzValue = parseInt(buzzValue);
     
     //Check that the numbers are integers.
+        //This is checking if both values are integers.  If true, continue into loop.
     if(Number.isInteger(fizzValue)  && Number.isInteger(buzzValue)){
-        //we call generateNumbers
-        let numbers = generateNumbers(fizzValue, buzzValue);
-        //we call displayNumbers
-        displayNumbers(numbers);
 
+        //callfizzBuzz
+        let fbArray = FizzBuzz(fizzValue, buzzValue);
+     
     } else {
         alert("You must enter numbers")
-    }
-
-    //We call fizzBuzz
-
-    //Call displayData and write values to the screen.
+    }   
     
 }
-
+//Start of FizzBuzz function, called from if loop.
 function fizzBuzz(fizzValue, buzzValue) {
     
     //Initialize the returnArray
     let returnArray = [];
+
     //loop from 1 to 100.
+    for (let i = 1; index <= 100; i++)1{
+          //Check the current in three steps.
+        //Check to see if divisible by Both (3 and 5)
+        //If so, push 'FizzBuzz' into an array and not the number.
+        if(i % fizzValue == 0 && i % buzzValue == 0){
+            
+            returnArray.push('FizzBuzz');
 
-    //Check the current in three steps.
-    //Check to see if divisible by Both (3 and 5)
-    //If so, push 'FizzBuzz' into an array and not the number.
+            //Check to see if divisible by fizz value (3)
+            //If so, push 'Fizz' into an array and not the number.
+        }else if (i % fizzValue == 0){
 
-    //Check to see if divisible by fizz value (3)
-    //If so, push 'Fizz' into an array and not the number.
-    
-    //Check to see if divisible by buzz value (5)
-    //If so, push 'Buzz' into an array and not the number.
-    
-    //If none, then push the number into the array.
+            returnArray.push('Fizz');
+            //Check to see if divisible by buzz value (5)
+            //If so, push 'Buzz' into an array and not the number.
+        }else if (i % buzzValue == 0){
+
+            returnArray.push('Buzz');
+
+            //If none, then push the number into the array.
+        }else{
+            
+            returnArray.push(i);
+
+        }        
+    }
 
     returnArray;
 
